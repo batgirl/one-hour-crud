98f297d0a719cfd2a35b91d3f8dae1f4428305d0
Chloe Alnaji <caedeal@gmail.com>
2015-10-27 09:28:25 -0600

    Set up RESTful routes in 18 minutes

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/planets', routes);
+app.use('/', function(req, res, next) {
+  res.redirect('/planets')
+});
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,41 @@ var db = require('monk')('localhost/one-hour-crud');
 var Planets = db.get('planets');
 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Planets.find({}, function(err, docs) {
+    res.json(docs);
+  }
 });
 
-router.
+router.get('/new', function(req, res, next) {
+  res.render()
+});
+
+router.post('/new', function(req, res, next) {
+  Planets.insert({name: req.body.name}, function(err, doc) {
+    res.json(doc);
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  Planets.findOne({_id: req.params.id}, function(err, doc) {
+    res.json(doc);
+  })
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  res.render()
+});
+
+router.post('/:id/edit', function(req, res, next) {
+  Planets.update({_id: req.params.id}, {name: req.body.name}, function(err, doc) {
+    res.json(doc);
+  })
+});
+
+router.post('/:id/delete', function(req, res, next) {
+  Planets.remove({_id: req.params.id}, function(err, doc) {
+    res.redirect('/');
+  })
+})
 
 module.exports = router;
